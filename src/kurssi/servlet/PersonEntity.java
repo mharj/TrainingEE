d1fdf04caa3199e207163ffa16851948ce305d81
Marko Harjula <marko.harjula@gmail.com>
2014-06-25 13:06:19 +0300

    Create PersonEntity.java

---
+++ src/kurssi/servlet/PersonEntity.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kurssi.servlet;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Oppi
+ */
+@Entity
+@TableGenerator(name="USER_SEQ",table = "SEQUENCE",pkColumnName = "SEQ_NAME")  
+@Table(name="message_person")
+public class PersonEntity implements Serializable {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.TABLE, generator="USER_SEQ")
+    private Long personId;    
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String passwd;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date registered;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastLogin;
+
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public Date getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(Date registered) {
+        this.registered = registered;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+    
+}
